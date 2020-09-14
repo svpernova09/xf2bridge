4e14744602a2b61ab4c5a0eba04891b84f7fac90
Joe Ferguson <joe@joeferguson.me>
2020-09-13 21:15:21 -0500

    Add missing exception?

---
+++ src/XF2Bridge/Visitor/AuthenticationException.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class AuthenticationException extends Exception
+{
+    /**
+     * Report the exception.
+     *
+     * @return void
+     */
+    public function report()
+    {
+        return false;
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        return response('Remember Tokens not implemented by '.get_class($this));
+    }
+}
